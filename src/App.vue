cd55d1b761de1c577175b46e99bc410fd0b189b7
TomeuMut <tomeu95@hotmail.com>
2022-04-01 19:09:40 +0200

    Git Init

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import HelloWorld from "./components/HelloWorld.vue";
+  import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="container mx-auto">
+     <div class="bg-white">
+      <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div class="text-center">
+          <h1 class="text-base font-semibold text-indigo-600 tracking-wide uppercase">Bartomeu Mut Vidal</h1>
+          <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">I'm a Frontend Developer</p>
+          <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Start building for free, then add a site plan to go live. Account plans unlock additional features.</p>
+        </div>
+      </div>
+    </div>   
+  </div>
+  <div class="container mx-auto">
+    <div class="columns-4 font-sans text-lg">
+      <div class="">
+        <img class="rounded-lg" src="https://source.unsplash.com/300x200/?office" alt="">
+        <h3>Project 1</h3>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quasi facilis. Veniam obcaecati nulla veritatis incidunt laudantium, perspiciatis alias consequatur accusamus consequuntur quaerat? Eligendi esse ut delectus obcaecati praesentium pariatur!
+        </div>
+      </div>
+      <div>
+        <img class="rounded-lg" src="https://source.unsplash.com/300x200/?office" alt="">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quasi facilis. Veniam obcaecati nulla veritatis incidunt laudantium, perspiciatis alias consequatur accusamus consequuntur quaerat? Eligendi esse ut delectus obcaecati praesentium pariatur!
+        </div>
+      </div>
+      <div>
+        <img class="rounded-lg" src="https://source.unsplash.com/300x200/?office" alt="">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quasi facilis. Veniam obcaecati nulla veritatis incidunt laudantium, perspiciatis alias consequatur accusamus consequuntur quaerat? Eligendi esse ut delectus obcaecati praesentium pariatur!
+        </div>
+      </div>          
+      <div>
+        <img class="rounded-lg" src="https://source.unsplash.com/300x200/?office" alt="">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, quasi facilis. Veniam obcaecati nulla veritatis incidunt laudantium, perspiciatis alias consequatur accusamus consequuntur quaerat? Eligendi esse ut delectus obcaecati praesentium pariatur!
+        </div>
+      </div>
+    </div>    
+  </div>
+</template>
